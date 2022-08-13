e928b35f8edc14b915b1007953c9375830f19d22
Adam <adamfingol@gmail.com>
2022-08-13 14:51:26 -0300

    feature: exemplo capitulo 2, código limpo

--- doc/capitulo_2.md
+++ doc/capitulo_2.md
@@ -4,6 +4,7 @@
 Como deixar o código mais amigáveis e informativo
 
     * Cabeçalho inicial com uma explicação geral do funcionamento.
+        [nota] Pode se criar um arquivo como Readme no lugar.]
     * Código alinhado e bem espaçado verticalmente.
     * Comentários explicativos e esclarecedores.
     * Nomes descritivos para funções e variáveis.
@@ -19,4 +20,12 @@ Como deixar o código mais amigáveis e informativo
     * Base de problemas conhecidos.
     * Versões beta.
     * Arquivo de exemplo de comportamento (se aplicável).
+
+# Código Limpo
+    
+    * Colocar apenas um comando por linha.
+    * Alinhar verticalmente comandos de um mesmo bloco, usando o espaços ou TAB's
+    * Deslocar o alinhamento à direita a cada bloco novo.
+    * Usar linhas em branco para separar trechos.
+    * Não ultrapassar o limite de 80 colunas por linha
 
---
+++ exemplo/C02_programa_misterioso_3.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# 
+# regex para localizar e subistituir %s/\(.*\)\(_2\)/\1_3/g
+#
+# C02_programa_misterioso_3.sh
+# 
+# O programa recebe dois números e mostra na saida padrão todos os números
+# existentes entre eles, inclusive. Essa sequência está pronta para ser 
+# usada pelo comando FOR. Casi inutudi i Número inicial, é utilizado o 1.
+# 
+# Exemplo 1                       
+# 
+# C02_programa_misterioso_3 5 10
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 
+# Exemplo 2      
+# 
+# C02_programa_misterioso_3.sh 10 5
+# 10
+# 9
+# 8
+# 7
+# 6
+# 5
+# 
+# Exemplo 3
+# 
+# C02_programa_misterioso_3.sh 5
+# 1
+# 2
+# 3
+# 4
+# 5
+# 
+# uso em script: for i in $(C02_programa_misterioso_3 10); do comandos ; done
+# 
+
+o=+ 
+a=1 
+z=${1:-1}
+
+[ "$2" ] && { 
+    a=$1 
+    z=$2 
+}  
+
+[ $a -gt $z ] && o=-
+
+while [ $a -ne $z ]; do 
+    echo $a 
+    eval "a=\$((a $o 1))"
+done
+
+echo $a
+
