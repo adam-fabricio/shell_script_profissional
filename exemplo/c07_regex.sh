1a1837c59c86c083a28f7bd6265fa21371372949
Adam <adam@adam.com>
2022-09-25 14:16:05 -0300

    feature: V3.0 - uso de lista [incompleto]

--- exemplo/c07_regex.sh
+++ exemplo/c07_regex.sh
@@ -7,11 +7,18 @@
 #
 #------------------------Historico--------------------------------------------#
 #
-#	v1.0 - 25/10/2022, Adam:
-#		- Uso do "^"
+#	V1.0	- 25/10/2022,	Adam:
+#		- Uso normal do grep
+#	v2.0	- 25/10/2022, 	Adam:
+#		- Uso do circunflexo "^"
+#	V3.0	- 25/10/2022,	Adam:
+#		- Uso do Cifrão "$"
+#	v4.0	- 25/10/2022,	Adam:
+#		- Uso de lista []
 #
 #------------------------Teste------------------------------------------------#
 # bash 5.1.16
+#
 #
 #------------------------Variavveis-------------------------------------------#
 # 
@@ -20,6 +27,6 @@
 echo -e "\033c"
 
 [ -z $1 ] && echo "Favor inserir parâmetro"
-[ "$1" = "1" ] && echo "$( grep root /etc/passwd	)"
-[ "$1" = "2" ] && echo "$( grep ^root /etc/passwd	)"
-
+[ "$1" = "1" ] && echo "$( grep root /etc/passwd	)"	#  todas linhas root
+[ "$1" = "2" ] && echo "$( grep ^root /etc/passwd	)"	#  apenas iniciadas
+[ "$1" = "3" ] && echo "$( grep bash$ /etc/passwd	)"	#  finalizadas
