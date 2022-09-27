9d2b2a486e667a4c7e6ca81fbd08a4bf29df789f
Adam <adam@adam.com>
2022-09-27 05:34:49 -0300

    feature: Continuando o capitulo 7 regex

--- exemplo/c07_regex.sh
+++ exemplo/c07_regex.sh
@@ -14,7 +14,13 @@
 #	V3.0	- 25/10/2022,	Adam:
 #		- Uso do Cifrão "$"
 #	v4.0	- 25/10/2022,	Adam:
+#		- Filtro por linhas vazias
+#	V5.0	- 27/10/2022,	Adam
 #		- Uso de lista []
+#	v6.0	- 27/10/2022,	Adam:
+#		- Uso de lista negada[^]
+#	v7.0	- 28/10.2022,	Adam:
+#		- Uso do ponto "."
 #
 #------------------------Teste------------------------------------------------#
 # bash 5.1.16
@@ -27,6 +33,10 @@
 echo -e "\033c"
 
 [ -z $1 ] && echo "Favor inserir parâmetro"
-[ "$1" = "1" ] && echo "$( grep root /etc/passwd	)"	#  todas linhas root
-[ "$1" = "2" ] && echo "$( grep ^root /etc/passwd	)"	#  apenas iniciadas
-[ "$1" = "3" ] && echo "$( grep bash$ /etc/passwd	)"	#  finalizadas
+[ "$1" = "1" ] && echo "$( grep root /etc/passwd	 	)"	
+[ "$1" = "2" ] && echo "$( grep ^root /etc/passwd	 	)"	
+[ "$1" = "3" ] && echo "$( grep bash$ /etc/passwd	 	)"	
+[ "$1" = "4" ] && echo "$( grep '^$' /etc/passwd	 	)"	
+[ "$1" = "5" ] && echo "$( grep 'd-[nr]' /etc/passwd 	)" 
+[ "$1" = "6" ] && echo "$( grep 'd-[^nr]' /etc/passwd	)"	
+[ "$1" = "7" ] && echo "$( grep '^.[aeiou]' /etc/passwd	)" 
