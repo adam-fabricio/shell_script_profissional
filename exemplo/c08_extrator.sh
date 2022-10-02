3e4955fbd5d9bc953485816ef7da170815d9752d
Adam <adam@adam.com>
2022-10-02 09:09:55 -0300

    feature: teste do script 1 em outro arquivo

--- exemplo/c08_extrator.sh
+++ exemplo/c08_extrator.sh
@@ -10,8 +10,14 @@
 #----------------------------------Histórico----------------------------------#
 #
 #	v1.0	-	02/11/2022	-	Adam
-#		- Primeira tentativa de retirar o link
+#		- Retirando a linha com a palvra 
+#	v1.1	-	02/11/2022	-	Adam
+#		- Retirando o link através do cut
 #	v2.0	-	02/11/2022	-	Adam
+#		- Teste com do script 1 em outro arquivo
+#	v2.1	-	02/11/2022	-	Adam
+#		- Retirando o link atraves do cut
+#
 #
 #------------------------------------Teste------------------------------------#
 #
@@ -25,8 +31,30 @@ echo -e '\033c'
 #----------------------------------EXECUÇÃO-----------------------------------#
 
 [ -z $1 ] && echo "Favor inseir parâmetros"
-[ "$1" = "1" ] && echo "$( fgrep Google emplo/c08_pagino1.html
-[ "$2" = "2" ] && echo "$(
+
+while test -n "$1"; do
+	case "$1" in
+		"1")
+			pagina="exemplo/c08_pagina1.html"
+			linha=$(fgrep Google "$pagina")
+			[ "$2" = "1" ] && linha=$(echo "$linha" | cut -d \" -f 2) && shift
+			echo "$linha"
+			;;
+		"2")
+			pagina="exemplo/c08_pagina2.html"
+			linha=$(fgrep Google "$pagina")
+			[ "$2" = "1" ] && linha=$(echo "$linha" | cut -d \" -f 2) && shift
+			echo "$linha"
+			;;
+	esac
+	shift
+done
+
+
+
+#--------------------------------PÓS EXECUÇÂO---------------------------------#
+
+echo
 
 #-----------------------------------------------------------------------------#
 
