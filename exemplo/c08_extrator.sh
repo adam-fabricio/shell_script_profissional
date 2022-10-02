de686f0f1ec968099bc3529168565afeef23cf3e
Adam <adam@adam.com>
2022-10-02 10:12:03 -0300

    feature: restruturado o código para uso do case e mensagem de ajuda

--- exemplo/c08_extrator.sh
+++ exemplo/c08_extrator.sh
@@ -17,7 +17,10 @@
 #		- Teste com do script 1 em outro arquivo
 #	v2.1	-	02/11/2022	-	Adam
 #		- Retirando o link atraves do cut
-#
+#	v3.0	-	02/11/2022	-	Adam
+#		- Restruturado o código usando case
+#		- Criado ajuda para o uso
+#		- melhorando o filtro
 #
 #------------------------------------Teste------------------------------------#
 #
@@ -28,28 +31,44 @@
 echo -e '\033c'
 
 #----------------------------------Variaveis----------------------------------#
+
+linha="Nao passado a forma de filtro"
+mensagem_de_ajuda="
+Uso $(basename "$0") [opção 1] [opção 2] [opção 3]
+	OPÇÕES:
+		N/A:	Aparece a opção de ajuda
+	OPÇÂO 1	- Escolha do arquivo que será utilizado para o filtro:
+		1	c08_pagina1.html
+		2	c08_pagina2.html
+	Opção 2	- Escolha do filtro da linha
+		1	fgrep Google arquivo
+		2	fgrep '>Google<\\a> arquivo
+	opção 3	- Retirada o link da linha
+		1	cut -d \" -f 2
+		2	sed 's/.*href='// ; s/'.*//'
+"
+
 #----------------------------------EXECUÇÃO-----------------------------------#
 
-[ -z $1 ] && echo "Favor inseir parâmetros"
+[ -z $1 ] && echo "$mensagem_de_ajuda"	&& exit 0
 
-while test -n "$1"; do
-	case "$1" in
-		"1")
-			pagina="exemplo/c08_pagina1.html"
-			linha=$(fgrep Google "$pagina")
-			[ "$2" = "1" ] && linha=$(echo "$linha" | cut -d \" -f 2) && shift
-			echo "$linha"
-			;;
-		"2")
-			pagina="exemplo/c08_pagina2.html"
-			linha=$(fgrep Google "$pagina")
-			[ "$2" = "1" ] && linha=$(echo "$linha" | cut -d \" -f 2) && shift
-			echo "$linha"
-			;;
-	esac
-	shift
-done
 
+case "$1" in
+	"1")	pagina="exemplo/c08_pagina1.html"	;;
+	"2")	pagina="exemplo/c08_pagina2.html"	;;
+esac
+
+case "$2" in
+	"1")	linha=$(fgrep Google "$pagina")			;;
+	"2")	linha=$(fgrep '>Google<\a>' "$pagina")	;;
+esac
+
+case "$3" in
+	"1")	linha=$(echo "$linha" | cut -d \" -f 2)					;;
+	"2")	linha=$(echo "$linha" | sed 's/.*href="// ; s/".*//')	;;
+esac
+
+echo "$linha"
 
 
 #--------------------------------PÓS EXECUÇÂO---------------------------------#
